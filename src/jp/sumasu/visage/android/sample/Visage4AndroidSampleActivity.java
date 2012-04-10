915103aa2e1a6a800f7d29f0b027fa92d06ab518
ryosms <ryo.sms@gmail.com>
2012-04-10 22:21:53 +0900

    Create Repository
    "A Visage Android application starts as a normal Android application project."

---
+++ src/jp/sumasu/visage/android/sample/Visage4AndroidSampleActivity.java
@@ -0,0 +1,13 @@
+package jp.sumasu.visage.android.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Visage4AndroidSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
